6719e2e6e0bad77c11561cb2ed1e7f97b8be57ff
Reema Chintala <rechinta@ucsc.edu>
2023-10-26 00:21:49 +0000

    graph ADT is working

--- pa3/Graph.c
+++ pa3/Graph.c
@@ -17,12 +17,16 @@ typedef struct GraphObj{
     List *neighbors;
     char *color;
     int *parent;
-    int *distance;
+
+    int *discover;
+    int *finish;
+
+
     
 
     int order; //vertices
     int size; //edges
-    int source;
+    
    
 } GraphObj;
 
@@ -36,8 +40,6 @@ Graph newGraph(int n){
     assert( G!=NULL );
     G -> size = 0;
     G -> order = n;
-    G -> source = NIL;
-
 
     G -> neighbors = (List*)calloc(n+1, sizeof(List));
 
@@ -45,23 +47,29 @@ Graph newGraph(int n){
     for (int i = 0; i < n+1; i++){
         G->neighbors[i] = newList();
     }
-    
 
 
     G -> color = (char*)calloc(n+1, sizeof(char));
     G -> parent = (int*)calloc(n+1, sizeof(int));
-    G -> distance = (int*)calloc(n+1, sizeof(int));
-    
+
+    G -> discover = (int*)calloc(n+1, sizeof(int));
+    G -> finish = (int*)calloc(n+1, sizeof(int));
+
+    for (int i = 0; i < n+1; i++){
+        G->discover[i] = UNDEF;
+        G->finish[i] = UNDEF;
+    }
+
+
 
     return(G);
 
 }
+
+
 void freeGraph(Graph* pG){
     if( pG!=NULL && *pG!=NULL ){
 
-        
-
-        
 
         for (int i = 0; i < ((*pG)->order) +1; i++){
             if ((*pG)->neighbors[i] != NULL){
@@ -73,7 +81,8 @@ void freeGraph(Graph* pG){
         free((*pG)->neighbors);
         free((*pG)->color);
         free((*pG)->parent);
-        free((*pG)->distance);
+        free((*pG)->discover);
+        free((*pG)->finish);
 
         
         free(*pG);
@@ -104,15 +113,7 @@ int getSize(Graph G){
 
 }
 
-int getSource(Graph G){
-    if( G==NULL ){
-      printf("Graph Error: calling getSource() on NULL Graph reference\n");
-      exit(EXIT_FAILURE);
-    }
 
-    return G->source;
-    
-}
 
 
 int getParent(Graph G, int u){
@@ -128,85 +129,53 @@ int getParent(Graph G, int u){
     }
 
 
-    if (getSource(G) == NIL){
-        return NIL;
-    }
     return G->parent[u];
 
 
 }
 
 
-int getDist(Graph G, int u){
 
+
+
+
+int getDiscover(Graph G, int u){
     if( G==NULL ){
-      printf("Graph Error: calling getDist() on NULL Graph reference\n");
+      printf("Graph Error: calling getDiscover() on NULL Graph reference\n");
       exit(EXIT_FAILURE);
     }
 
     if(u < 1 || u > getOrder(G)){
-      printf("Graph Error: calling getDist() when 1 ≤ u ≤ getOrder(𝐺) not met\n");
+      printf("Graph Error: calling getDiscover() when 1 ≤ u ≤ getOrder(𝐺) not met\n");
       exit(EXIT_FAILURE);
     }
 
-    if (getSource(G) == NIL){
-        return INF;
-    }
+    return G->discover[u];
 
-    return G->distance[u];
-}
+} /* Pre: 1<=u<=n=getOrder(G) */
 
-void getPath(List L, Graph G, int u){
 
+int getFinish(Graph G, int u){
     if( G==NULL ){
-        printf("Graph Error: calling getPath() on NULL Graph reference\n");
-        exit(EXIT_FAILURE);
+      printf("Graph Error: calling getFinish() on NULL Graph reference\n");
+      exit(EXIT_FAILURE);
     }
 
     if(u < 1 || u > getOrder(G)){
-      printf("Graph Error: calling getPath() when 1 ≤ u ≤ getOrder(𝐺) not met\n");
+      printf("Graph Error: calling getFinish() when 1 ≤ u ≤ getOrder(𝐺) not met\n");
       exit(EXIT_FAILURE);
     }
 
-    
-    if(getSource(G)== NIL){
-        printf("Graph Error: calling getPath() without calling BFS() before\n");
-        exit(EXIT_FAILURE);
-    }
+    return G->finish[u];
+} /* Pre: 1<=u<=n=getOrder(G) */
 
 
-    if (u == getSource(G)){
-        append(L, getSource(G));
-    }
-        
-    else if (G->parent[u] == NIL){
-        append(L, NIL);
-    }
-    else{
-        getPath(L, G, G->parent[u]);
-        append(L, u);
-    }
-}
 
 
 /*** Manipulation procedures ***/
 
 
-void makeNull(Graph G){
-    if( G==NULL ){
-      printf("Graph Error: calling makeNull() on NULL Graph reference\n");
-      exit(EXIT_FAILURE);
-    }
 
-    for (int i = 1; i < getOrder(G)+1; i++){
-        if (G->neighbors[i] != NULL){
-            clear(G->neighbors[i]);
-        }
-    }
-
-    G->size = 0;
-
-}
 
 
 void addEdge(Graph G, int u, int v){
@@ -283,88 +252,137 @@ void addArc(Graph G, int u, int v){
       exit(EXIT_FAILURE);
     }
 
+    
+    
+
     if (length(G->neighbors[u]) == 0){
         append(G->neighbors[u], v);
+        G->size ++;
+        
     }
     else{
 
-        moveBack(G->neighbors[u]);
-        while(1){
-            if (v > get(G->neighbors[u])){
-                insertAfter(G->neighbors[u], v);
-                break;
-            }  
-            else if (index(G->neighbors[u]) == 0) {
-                insertBefore(G->neighbors[u], v);
-                break;     
+        int check = 1;
+        moveFront(G->neighbors[u]);
+        
+        while (index(G->neighbors[u]) != -1){
+            if (get(G->neighbors[u]) == v){
+                check = 0;
             }
-            else {
-                movePrev(G->neighbors[u]);
-            }   
+            moveNext(G->neighbors[u]);
+        }
+
+        if (check == 1){
+
+            moveBack(G->neighbors[u]);
+            while(1){
+                if (v > get(G->neighbors[u])){
+                    insertAfter(G->neighbors[u], v);
+                    break;
+                }  
+                else if (index(G->neighbors[u]) == 0) {
+                    insertBefore(G->neighbors[u], v);
+                    break;     
+                }
+                else {
+                    movePrev(G->neighbors[u]);
+                }   
+            }
+            
+            G->size ++;
         }      
     }
-
-    G->size ++;
-}
-
-
-void BFS(Graph G, int s){
-    if( G==NULL ){
-      printf("Graph Error: calling BFS() on NULL Graph reference\n");
-      exit(EXIT_FAILURE);
-    }
-
-    G->source = s;
-
-    for (int i = 1; i < getOrder(G) + 1; i ++){
-
-        if (i != s){
-            G->color[i] = 'w';
-            G->distance[i] = INF;
-            G->parent[i] = NIL;
-        }
-    } 
-
-    G->color[s] = 'g';       // discover the source s
-    G->distance[s] = 0;
-    G->parent[s] = NIL; 
-
-    List Q = newList();              // construct a new empty queue
-    append(Q,s);
-
-
-    while (length(Q) != 0){
-
-        int x = front(Q);
-        deleteFront(Q);
-
-        moveFront(G->neighbors[x]);
-
-        for (int i = 0; i < length((G->neighbors)[x]); i++){
-
-            
-            int y = get(G->neighbors[x]);
-
-            if (G->color[y] == 'w' ){        // y is undiscovered
-                G->color[y] = 'g';         // discover y
-                G->distance[y] = getDist(G, x) + 1; 
-                G->parent[y] = x;
-                append(Q,y);
-            } 
-
-            moveNext(G->neighbors[x]);
-        }
-        G->color[x] = 'b';                  // finish x
-
-        
-    }
-    freeList(&Q);
 
     
 }
 
 
+
+
+void Visit(Graph G, int x, int* time, List S){
+    
+    G->discover[x] = ++(*time);        // discover x
+    G->color[x] = 'g';
+    List adj = G->neighbors[x];
+    moveFront(adj);
+    for (int i = 0; i <length(adj); i++){
+        if (G->color[get(adj)] == 'w'){
+            G->parent[get(adj)] = x;
+            Visit(G, get(adj), time, S);
+        }
+        moveNext((G->neighbors[x]));
+    }
+    G->color[x] = 'b';
+    G->finish[x] = ++(*time);        // finish x
+    insertAfter(S, x);
+
+}
+
+void DFS(Graph G, List S){
+
+    for (int i = 1; i < getOrder(G) + 1; i ++){
+        G->color[i] = 'w';
+        G->parent[i] = NIL;
+    } 
+    
+    int time = 0;
+
+    moveBack(S);
+
+    while (time < getOrder(G)*2){
+
+        if (G->color[front(S)] == 'w'){
+
+            Visit(G, front(S), &time, S);
+        }
+        deleteFront(S);
+        
+    }
+
+    while (length(S) > getOrder(G)){
+        deleteFront(S);
+    }
+
+    
+}
 /*** Other operations ***/
+
+Graph copyGraph(Graph G){
+
+    Graph N = newGraph(getOrder(G));
+
+    for (int i = 1; i < getOrder(G)+1; i++){
+        moveFront(G->neighbors[i]);
+        while (index(G->neighbors[i]) != -1){
+            addArc(N, i, get(G->neighbors[i]));
+            moveNext(G->neighbors[i]);
+        }
+    }
+
+    return N;
+
+    
+
+}
+
+
+Graph transpose(Graph G){
+
+    Graph T = newGraph(getOrder(G));
+
+    for (int i = 1; i < getOrder(G)+1; i++){
+        moveFront(G->neighbors[i]);
+        while (index(G->neighbors[i]) != -1){
+
+            addArc(T, get(G->neighbors[i]), i);
+            moveNext(G->neighbors[i]);
+        }
+    }
+    return T;
+}
+
+
+
 void printGraph(FILE* out, Graph G){
     if( G==NULL ){
       printf("Graph Error: calling printGraph() on NULL Graph reference\n");
--- pa3/Graph.h
+++ pa3/Graph.h
@@ -3,8 +3,9 @@ rechinta
 PA2*/
 
 #include "List.h"
-#define INF -1
-#define NIL -2
+
+#define NIL 0
+#define UNDEF -10
 
 typedef struct GraphObj* Graph;
 
@@ -21,10 +22,22 @@ int getSource(Graph G);
 int getParent(Graph G, int u);
 int getDist(Graph G, int u);
 void getPath(List L, Graph G, int u);
+
+int getDiscover(Graph G, int u); /* Pre: 1<=u<=n=getOrder(G) */
+int getFinish(Graph G, int u); /* Pre: 1<=u<=n=getOrder(G) */
+
+
+
 /*** Manipulation procedures ***/
 void makeNull(Graph G);
 void addEdge(Graph G, int u, int v);
 void addArc(Graph G, int u, int v);
 void BFS(Graph G, int s);
+
+void DFS(Graph G, List S); /* Pre: length(S)==getOrder(G) */
+
 /*** Other operations ***/
-void printGraph(FILE* out, Graph G);
+
+Graph transpose(Graph G);
+Graph copyGraph(Graph G);
+void printGraph(FILE* out , Graph G);
