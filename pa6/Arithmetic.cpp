1188e8ec0e7c56cf5868ab1507ac07c2c1e77481
Reema Chintala <rechinta@ucsc.edu>
2023-11-22 12:02:38 -0800

    adding arithmetic

--- pa6/Arithmetic.cpp
+++ pa6/Arithmetic.cpp
@@ -1,40 +1,94 @@
 #include<iostream>
 #include<string>
 #include<stdexcept>
+#include <fstream>
 #include"BigInteger.h"
 
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
+    
+    if( argc != 3 ){
+      printf("Usage: %s input output\n", argv[0]);
+      exit(1);
+    }
 
-    string s1 = "-0000000000041085449";
-    string s2 = "6402779357";
+    std::string in = argv[1];
+
+    std::string out = argv[2];
+
+    std::ifstream inputFile(in);
+    std::ofstream outputFile(out);
+
+    std::string s1;
+    std::string s2;
+
+    if (inputFile.is_open()) {
+        
+        std::getline(inputFile, s1);
+
+        // Skip the newline character
+        inputFile.ignore();
+
+        // Read the second line and store it in another string variable
+        
+        std::getline(inputFile, s2);
+
+        // Close the file when done
+        inputFile.close();
+
+        
+    } else {
+        std::cerr << "Unable to open the file: " << in << std::endl;
+    }
+
+    
+    
     
     BigInteger A = BigInteger(s1);
     BigInteger B = BigInteger(s2);
 
-    cout << A << endl;
-    cout << endl;
-    cout << B << endl;
-    cout << endl;
-    cout << A+B << endl;
-    cout << endl;
-    cout << A-B << endl;
-    cout << endl;
-    cout << A-A << endl;
-    cout << endl;
 
-    cout << (BigInteger(3)*A)-(BigInteger(2)*B) << endl;
-    cout << endl;
+    if (outputFile.is_open()) {
 
-    cout << A*B << endl;
-    cout << endl;
+        outputFile << A << endl;
+        outputFile << endl;
+        outputFile << B << endl;
+        outputFile << endl;
+        outputFile << A+B << endl;
+        outputFile << endl;
+        outputFile << A-B << endl;
+        outputFile << endl;
+        outputFile << A-A << endl;
+        outputFile << endl;
 
-    cout << A*A << endl;
-    cout << endl;
+        outputFile << (BigInteger(3)*A)-(BigInteger(2)*B) << endl;
+        outputFile << endl;
 
-    cout << B*B << endl;
-    cout << endl;
+        outputFile << A*B << endl;
+        outputFile << endl;
+
+        outputFile << A*A << endl;
+        outputFile << endl;
+
+        outputFile << B*B << endl;
+        outputFile << endl;
+
+        outputFile << (BigInteger(9)*A*A*A*A)+(BigInteger(16)*B*B*B*B*B) << endl;
+        outputFile << endl;
+        
+        outputFile.close();
+    
+    } else {
+        std::cerr << "Unable to open the file: " << out << std::endl;
+    }
+
+
+   
+
+
+    
+
 
     
 
